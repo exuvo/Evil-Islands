bed9c4f62de306c779c3e1dc4848c2a20188f199
exuvo <exuvo@exuvo.se>
2012-10-08 14:45:16 +0200

    Removed event listener from Network.

--- Evil Network/src/se/exuvo/evil/shared/world/EventListenerr.java
+++
@@ -1,9 +0,0 @@
-package se.exuvo.evil.shared.world;
-
-import java.util.EventListener;
-import java.util.EventObject;
-
-public interface EventListenerr extends EventListener{
-	public void event(EventObject e);
-
-}
